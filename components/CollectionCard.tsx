67b2318c73154d273ea403db45813704605b2a06
RadityaDito <raditya.ihsan@ui.ac.id>
2023-09-28 12:49:11 +0700

    Add delete task function and finishing collection card

--- components/CollectionCard.tsx
+++ components/CollectionCard.tsx
@@ -1,21 +1,61 @@
 "use client";
 
-import { createCollectionSchemaType } from "@/schema/createCollection";
 import { Collection } from "@prisma/client";
-import { FC, useState } from "react";
-import { Collapsible } from "./ui/collapsible";
-import { CollapsibleTrigger } from "@radix-ui/react-collapsible";
+import { FC, useState, useTransition } from "react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
 import { Button } from "./ui/button";
 import { cn } from "@/lib/utils";
 import { CollectionColor, CollectionColors } from "@/lib/constants";
-import { CaretDownIcon, CaretUpIcon } from "@radix-ui/react-icons";
+import { CaretDownIcon, CaretUpIcon, TrashIcon } from "@radix-ui/react-icons";
+import { Progress } from "./ui/progress";
+import { Separator } from "./ui/separator";
+import PlusIco from "./icons/PlusIco";
+import {
+  AlertDialogCancel,
+  AlertDialogFooter,
+  AlertDialogTrigger,
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogAction,
+  AlertDialogTitle,
+  AlertDialogDescription,
+} from "./ui/alert-dialog";
+import { deleteCollection } from "@/actions/collection";
+import { toast } from "./ui/use-toast";
+import { useRouter } from "next/navigation";
 
 interface CollectionCardProps {
   collection: Collection;
 }
 
+const tasks = ["task 1", "task 2"];
+
 const CollectionCard: FC<CollectionCardProps> = ({ collection }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+
+  const [isLoading, startTranstition] = useTransition();
+
+  const removeCollection = async () => {
+    try {
+      await deleteCollection(collection.id);
+      toast({
+        title: "Success",
+        description: "Collection has been deleted successfully",
+      });
+      router.refresh();
+    } catch (error: any) {
+      toast({
+        title: "Error",
+        description: "Cannot delete collection",
+        variant: "destructive",
+      });
+    }
+  };
   return (
     <Collapsible open={isOpen} onOpenChange={setIsOpen}>
       <CollapsibleTrigger asChild>
@@ -23,6 +63,7 @@ const CollectionCard: FC<CollectionCardProps> = ({ collection }) => {
           variant={"ghost"}
           className={cn(
             "flex w-full justify-between p-6",
+            isOpen && "rounded-b-none",
             CollectionColors[collection.color as CollectionColor]
           )}
         >
@@ -31,6 +72,55 @@ const CollectionCard: FC<CollectionCardProps> = ({ collection }) => {
           {isOpen && <CaretUpIcon className="h-6 w-6" />}
         </Button>
       </CollapsibleTrigger>
+      <CollapsibleContent className="flex rounded-b-md flex-col dark:bg-neutral-900 shadow-lg ">
+        {tasks.length === 0 && <div>No Tasks</div>}
+        {tasks.length > 0 && (
+          <>
+            <Progress value={45} className="rounded-none" />
+            <div className="p-4 gap-3 flex flex-col">
+              {tasks.map((task) => (
+                <div className="">Mocked task</div>
+              ))}
+            </div>
+          </>
+        )}
+        <Separator />
+        <footer className="h-[40px] px-4 p-[2px] text-xs text-neutral-500 flex items-center justify-between">
+          <p>Created at {collection.createdAt.toLocaleDateString("id-ID")}</p>
+          {isLoading && <div>Deleting...</div>}
+          {!isLoading && (
+            <div>
+              <Button size={"icon"} variant={"ghost"}>
+                <PlusIco />
+              </Button>
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <Button size={"icon"} variant={"ghost"}>
+                    <TrashIcon />
+                  </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action cannot be undone. This will be permanently
+                    delete your collection and task inside it
+                  </AlertDialogDescription>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction
+                      onClick={() => {
+                        startTranstition(removeCollection);
+                      }}
+                    >
+                      Proceed
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+            </div>
+          )}
+        </footer>
+      </CollapsibleContent>
     </Collapsible>
   );
 };
---
+++ components/icons/PlusIco.tsx
@@ -0,0 +1,20 @@
+const PlusIco = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className="w-4 h-4"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12 4.5v15m7.5-7.5h-15"
+      />
+    </svg>
+  );
+};
+
+export default PlusIco;
